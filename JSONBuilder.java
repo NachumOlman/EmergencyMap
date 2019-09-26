e13e0b81081ba139a82a0aaa8b9bec8cb84464f5
guyguy40 <46048002+guyguy40@users.noreply.github.com>
2019-09-26 13:27:00 +0300

    Update JSONBuilder.java

--- JSONBuilder.java
+++ JSONBuilder.java
@@ -19,25 +19,19 @@ public class JSONBuilder {
         STT SpeechToText = new STT();
         Classifier clf = new Classifier();
         String fullFile = SpeechToText.VoiceToText(fileName);
-        System.out.println(fullFile);
-        System.out.println("---");
         String toReplace = "hello this is an automated receptionist said because due to a back log on the emergency line I will ask you a few questions what is the disaster";
         String startCut = fullFile.replace(toReplace,"");
-        System.out.println(startCut);
-        System.out.println("---");
         String[] tokens = startCut.split("what is your current location");
-        System.out.println(tokens[0]);
-        System.out.println("---");
         String fullDesc = tokens[0];
         String type = null;
-        //try {type = clf.classifyDisaster(tokens[0]);}
-        //catch(Exception e) {e.printStackTrace();}
+        try {type = clf.classifyDisaster(tokens[0]);}
+        catch(Exception e) {e.printStackTrace();}
         tokens = tokens[1].split("");
         String location = tokens[0];
         tokens = tokens[1].split("");
         boolean injured = false;
-        //try {injured = clf.classifyInjured(tokens[0]);}
-        //catch(Exception e) {e.printStackTrace();}
+        try {injured = clf.classifyInjured(tokens[0]);}
+        catch(Exception e) {e.printStackTrace();}
         return buildFrom(phoneNum, location, type, injured, fullDesc);
     }
 }
